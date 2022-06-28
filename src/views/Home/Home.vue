3e64ba9aa42e4b3f3e6074a92eeb1697ea73c35e
“xinsir” <2418074134@qq.com>
2022-06-28 09:25:18 +0800

    新增about

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,44 +1,43 @@
 <template>
-    <div class="home">
-        <div class="home-header">
-            <div class="text trend-text">Trending</div>
-            <div class="flex-center">
-                <div class="text question-text">Questions</div>
-                <img src="../../assets/images/icon_downarrow.png" alt="">
-            </div>
-        </div>
-        <div class="home-main">
-            <div class="text main-title">Swipe left/right to check more</div>
-            <div class="main-tep">
-                <div class="tep-text"></div>
-            </div>
-        </div>
-        <div class="home-footer">111111</div>
+  <div class="home">
+    <div class="home-header">
+      <div class="text trend-text">Trending</div>
+      <div class="flex-center">
+        <div class="text question-text">Questions</div>
+        <img src="../../assets/images/icon_downarrow.png" alt="" />
+      </div>
     </div>
+    <div class="home-main">
+      <div class="text main-title">Swipe left/right to check more</div>
+      <div class="main-tep">
+        <div class="tep-text"></div>
+      </div>
+    </div>
+    <div class="home-footer">111111</div>
+  </div>
 </template>
 <style scoped>
-.home{
-    height: 100vh;
-    padding: 3.6rem;
-    background: linear-gradient(#316be9,#d0c6f8,#f4e8dc);
+.home {
+  height: 100vh;
+  padding: 3.6rem;
+  background: linear-gradient(#316be9, #d0c6f8, #f4e8dc);
 }
-.trend-text{
-    font-size: 2rem;
+.trend-text {
+  font-size: 2rem;
 }
-.question-text{
-    font-size: 3.5rem;
+.question-text {
+  font-size: 3.5rem;
 }
-.main-title{
-    font-size: 1rem;
-    border-radius: 4rem;
-    padding: 0.8rem 6rem;
-    background: #566aa9;
-    margin: 2.6rem 0 1.2rem 0 ;
+.main-title {
+  font-size: 1rem;
+  border-radius: 4rem;
+  padding: 0.8rem 6rem;
+  background: #566aa9;
+  margin: 2.6rem 0 1.2rem 0;
 }
-.tep-text{
-    background: #fff;
-    border-radius: 2rem;
-    height: 20rem;
+.tep-text {
+  background: #fff;
+  border-radius: 2rem;
+  height: 20rem;
 }
-
 </style>
