4deb2373fff848a7a0bdfa9b0484172e83d3570d
gcw <2900733724@qq.com>
2022-07-01 18:33:06 +0800

    轮播

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from "./router";
 import './assets/lib/amfe-flexible';
 import './common.css'
-
+import Vant from 'vant';
+import 'vant/lib/index.css';
 const app = createApp(App)
 app.use(router)
+app.use(Vant);
 app.mount('#app');
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,82 +1,56 @@
 <template>
-<div class="bgc">
-    <div class="home">
-        <div class="home-header">
-            <div class="text trend-text">Trending</div>
-            <div class="flex-center">
-                <div class="text question-text">Questions</div>
-                <img src="../../assets/images/icon_downarrow.png" alt="">
+    <div class="bgc">
+        <div class="home">
+            <div class="home-header">
+                <div class="text trend-text">Trending</div>
+                <div class="flex-center">
+                    <div class="text question-text">Questions</div>
+                    <img src="../../assets/images/icon_downarrow.png" alt="">
+                </div>
+            </div>
+            <div class="home-main">
+                <div class="text main-title">Swipe left/right to check more</div>
+                <div class="main-tep">
+                    <van-swipe>
+                        <van-swipe-item>
+                            <div class="tep-text">
+                                <div class="blue-text">#ui #ux #intoroction</div>
+                                <div class="bold-text main-text">Why do people clear the screen multiple times when
+                                    using a
+                                    calculator?</div>
+                                <div class="main-answers flex-center">
+                                    <div class="answer-text">10 answers</div>
+                                    <div class="answer-text">12k views</div>
+                                </div>
+                                <div class="icon_person flex-center">
+                                    <img class="icon mr-10" src="../../assets/images/icon_headsculpture.jpg" alt="">
+                                    <div class="">
+                                        <div class="bold-text user-text">Donot Twerski</div>
+                                        <div class="info-text">UK dessigret update</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </van-swipe-item>
+                        <van-swipe-item>2</van-swipe-item>
+                        <van-swipe-item>3</van-swipe-item>
+                        <van-swipe-item>4</van-swipe-item>
+                    </van-swipe>
+                </div>
             </div>
         </div>
-        <div class="home-main">
-            <div class="text main-title">Swipe left/right to check more</div>
-                <div class="main-tep">
-                    <div class="tep-one">
-                        <div class="tep-text">
-                            <div class="blue-text">#ui #ux #intoroction</div>
-                            <div class="bold-text main-text">Why do people clear the screen multiple times when using a
-                                calculator?</div>
-                            <div class="main-answers flex-center">
-                                <div class="answer-text">10 answers</div>
-                                <div class="answer-text">12k views</div>
-                            </div>
-                            <div class="icon_person flex-center">
-                                <img class="icon mr-10" src="../../assets/images/icon_headsculpture.jpg" alt="">
-                                <div class="">
-                                    <div class="bold-text user-text">Donot Twerski</div>
-                                    <div class="info-text">UK dessigret update</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="tep-two">
-                        <div class="tep-text">
-                            <div class="blue-text">#ui #ux #intoroction</div>
-                            <div class="bold-text main-text">Why do people clear the screen multiple times when using a
-                                calculator?</div>
-                            <div class="main-answers flex-center">
-                                <div class="answer-text">10 answers</div>
-                                <div class="answer-text">12k views</div>
-                            </div>
-                            <div class="icon_person flex-center">
-                                <img class="icon mr-10" src="../../assets/images/icon_headsculpture.jpg" alt="">
-                                <div class="">
-                                    <div class="bold-text user-text">Donot Twerski</div>
-                                    <div class="info-text">UK dessigret update</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-        </div>
-    </div>
         <div class="home-footer">
             <div class="flex-between">
-                <img class="icon-size"  src="../../assets/images/sort.png" alt="">
+                <img class="icon-size" src="../../assets/images/sort.png" alt="">
                 <img class="icon-size" src="../../assets/images/search.png" alt="">
                 <img class="icon-size" src="../../assets/images/add.png" alt="">
                 <img class="icon-size" src="../../assets/images/like.png" alt="">
                 <img class="icon-size" src="../../assets/images/mine.png" alt="">
             </div>
         </div>
-</div>
+    </div>
 </template>
 <style scoped>
 
-.tep-one,.tep-two{
-    background-color: #fff;
-    border-radius: 2rem;
-    position: absolute;
-    left: 0;
-    top: 0;
-    transition: transform 1s;
-}
-.tep-two{
-    top: 2rem;
-}
-.main-tep:hover .tep-one{
-    transform:translateY(5rem);
-}
 .bgc{
     background: #ccc;
 }
@@ -100,13 +74,11 @@
   margin: 2.6rem 0 1.2rem 0;
 }
 .main-tep{
-    position: relative;
-    /* background-color: #fff; */
-    /* border-radius: 2rem; */
-    height: 35rem;
+    background-color: #fff;
+    border-radius: 2rem;
+    padding: 3.5rem;
 }
 .tep-text{
-    margin: 3.5rem;
     background: url('../../assets/images/Q.png') no-repeat;
     background-size: 10rem;
     background-position: 13rem 20rem;
